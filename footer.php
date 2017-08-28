a253a1cfeffe5bcd1dc85afd413735a88d6d1301
Namanyay Goel <mail@namanyayg.com>
2017-08-28 13:54:22 +0530

    First commit

---
+++ footer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Pyaar
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer">
+		<div class="site-info">
+			Penned with love by <a href="http://namanyayg.com/" rel="nofollow designer">Namanyay Goel</a>.
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
